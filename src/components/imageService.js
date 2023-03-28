22259940d2699d7f9295407a6171859663a4e5c1
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-03-29 01:49:25 +0300

    instal axios

--- src/components/imageService.js
+++ src/components/imageService.js
@@ -1,7 +1,10 @@
 export default class ImageServiceAPI {
   constructor() {
     this.search_query = '';
-     this.page = 1; // increment number of page need to put om then(data)
+     this.page = 2; // increment number of page need to put om then(data)
+     this.markup ='';
+    
+
   }
 
   get query() {
@@ -15,24 +18,68 @@ export default class ImageServiceAPI {
   resetPage(){
      this.page = 1;
   }
+  incrementPage(){
+    this.page += 1;
+  }
 
   fetchImage() {
     const BASE_URL = 'https://pixabay.com/api/';
+    const key = '34586692-ed7cb8a238ccde585a263c879';
     const searchTypePhoto = '&image_type=photo';
     const searchOrientation = '&orientation="horizontal"';
     const ageFilter = '&safesearch="true"';
-    const key = '34586692-ed7cb8a238ccde585a263c879';
-
+    const perPage ='&per_page=40';
+    const page = `&page=${this.page}`;
+ 
+ 
    return fetch(
-      `${BASE_URL}?key=${key}&q=${this.search_query}${searchTypePhoto}${searchOrientation}${ageFilter}`
+      `${BASE_URL}?key=${key}&q=${this.search_query}${searchTypePhoto}${searchOrientation}${ageFilter}${perPage}${page}`
     )
-      .then(resp => resp.json)
+      .then(resp => resp.json())
       .then(data => {
-          // console.log(data);
-           return data;
-      }
-        
-        //this.page +=1 //increment number of page
-      );
+          return data;
+      });
   }
-}
+
+ 
+ makeGalleryCard(renderObject){
+   // {webformatURL,largeImageURL ,tags, likes, views, comments, downloads}
+   return this.markup = renderObject.map(({webformatURL, tags, likes, views, comments, downloads}) => {
+   
+     return   `<div class="photo-card">  
+     <div class="thumb">
+     <img class="img__card" src="${webformatURL}" alt="${tags}" loading="lazy" />
+     </div>                           
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${downloads}
+          </p>
+        </div>
+      </div>`
+   }).join('');
+ 
+ 
+  }
+
+ 
+  getReadyMarkup(){
+   return this.markup;
+  }
+  
+
+ }
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,107 @@
 // infinite-scroll.com
 import './common/style.css'
 import  ImageServiceAPI from './components/imageService'
-const imagesApi = new ImageServiceAPI(); //import Class
+import Notiflix from 'notiflix';
+const axios = require('axios').default;
 
+const imagesApi = new ImageServiceAPI(); //import Class
+let markupCards = '';
+let oldvalue = '';
 const refs = {
-    searchForm: document.querySelector('#search-form'), // link form
+   searchForm: document.querySelector('#search-form'), // link form
+   gallery: document.querySelector('.gallery'),
+   buttonLoadMore: document.querySelector('.load-more'),
+  
 }
+
 
 refs.searchForm.addEventListener('submit',handleSubmit); // catch value Submit
 
-function handleSubmit(e){ // event reac tion
-    e.preventDefault(); //cancel reload pade
+function handleSubmit(e){ 
+   e.preventDefault(); //cancel reload page
+   removeButtonLeadMore();
+   if(!e.currentTarget.elements.searchQuery.value){ //protect against non-entered data
+    Notiflix.Notify.failure(
+      'input Something please',
+      {
+        timeout: 6000,
+      },
+    );
+     return;
+   }
+   
+   if (oldvalue === e.currentTarget.elements.searchQuery.value){ //protect against the  same request 
+    Notiflix.Notify.info(
+      'Change the query value please',
+      {
+        timeout: 6000,
+      },
+    );
+     return;
+   }else {
+    oldvalue = e.currentTarget.elements.searchQuery.value;
+   }
+   
+ imagesApi.query = e.currentTarget.elements.searchQuery.value; //write value search
+ imagesApi.resetPage(); //reset number of find page
 
-  imagesApi.query = e.currentTarget.elements.searchQuery.value; //write value
-  imagesApi.resetPage();
-  imagesApi.fetchImage().then(baseData => {console.log(baseData)});
+ imagesApi.fetchImage()
+ .then(baseData => {
+   if(!baseData.hits.length) {
+    Notiflix.Notify.failure(          // send non blocking alert
+    'Sorry, there are no images matching your search query. Please try again.',
+      {
+        timeout: 6000,
+      },
+    ); 
+          return;
+   }
+     
+galleryAbortContainer();
+imagesApi.makeGalleryCard(baseData.hits);
+markupCards = imagesApi.getReadyMarkup();
+refs.gallery.insertAdjacentHTML("beforeend", markupCards); // render cards
+addButtonLeadMore(); //show buttom More
+ }).catch(error => console.log(error));
+ }
+
+refs.buttonLoadMore.addEventListener('click', onLoadMore);
+
+function onLoadMore(e){
+  removeButtonLeadMore();
+  imagesApi.incrementPage();
+  imagesApi.fetchImage()
+ .then(baseData => {
+  addButtonLeadMore();
+  imagesApi.makeGalleryCard(baseData.hits);
+ markupCards = imagesApi.getReadyMarkup();
+refs.gallery.insertAdjacentHTML("beforeend", markupCards);
+allContentLoaded(baseData);
+}).catch(error => console.log(error));
 }
 
-// function onLoadMore(searchQuery){
-// }
+function galleryAbortContainer(){
+  refs.gallery.innerHTML = '';
+}
+
+function addButtonLeadMore() {
+  refs.buttonLoadMore.classList.remove('visually-hidden');
+}
+
+function removeButtonLeadMore(){
+  refs.buttonLoadMore.classList.add('visually-hidden');
+}
+
+function allContentLoaded(allHits){
+  let cardCounter = document.querySelectorAll(".photo-card").length;
+  if(cardCounter >= allHits.totalHits){
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results.",
+      {
+        timeout: 6000,
+      },
+    );
+    removeButtonLeadMore();
+  
+  }
+}
